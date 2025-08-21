3d8558252eb027f78ee49c812889859de9994186
heloisa <depaulamariaheloisa@gmail.com>
2025-08-21 19:32:38 -0300

    alterei o try except

--- exececoesTryExcept.py
+++ exececoesTryExcept.py
@@ -1,7 +1,5 @@
-numero = input("Digite um número para mostrar o dobro: ")
-
 try: 
-    numero_float = float(numero)
+    numero_float = float(input("Informe um numero"))
     print("Float numero ", numero_float)
     print(f"O resultado é {numero_float * 2: .2f}")
 except:
