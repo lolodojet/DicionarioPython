eee185dc28ec3e325b16069890a5d282f927a639
heloisa <depaulamariaheloisa@gmail.com>
2025-08-21 19:48:16 -0300

    arrumei código de exceção

--- exececoesException.py
+++ exececoesException.py
@@ -3,6 +3,6 @@ nomes = ["Ana", "Carlos", "Maria"]
 for tentativa in range(3):
     try:
         i = int(input("Digite o índice que quer imprimir "))
-        print(nomes(i))
+        print(nomes[i])
     except Exception as e:
         print(f"Algo de errado aconteceu {e}")
